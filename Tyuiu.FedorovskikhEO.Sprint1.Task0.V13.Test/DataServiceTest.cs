a0a9e1b499b04d3f70aec19c2be084494e9006da
FedorovskikhKatya <katafedorovskih53@gmail.com>
2026-09-15 21:06:14 +0500

    Add project files.

---
+++ Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Lib;
+namespace Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.Equal(1, res);
+        }
+    }
+}
