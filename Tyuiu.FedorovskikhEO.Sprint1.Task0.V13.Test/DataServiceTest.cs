17c7bcb6c2559261d674ee5c18f6b3e65173f5ba
FedorovskikhKatya <katafedorovskih53@gmail.com>
2026-09-18 01:17:04 +0500

    Task0

--- Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Test/DataServiceTest.cs
+++ Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -1,14 +1,16 @@
 using Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Lib;
+
 namespace Tyuiu.FedorovskikhEO.Sprint1.Task0.V13.Test
 {
+    [TestClass]
     public class DataServiceTest
     {
-        [Fact]
+        [TestMethod]
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            var res = ds.Calculate ();
-            Assert.Equal(1, res);
+            var res = ds.Calculate();
+            Assert.AreEqual(1.0, res);
         }
     }
 }
