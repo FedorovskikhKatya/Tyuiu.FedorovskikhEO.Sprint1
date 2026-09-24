a94cb2ea48971d54a051bc55d216d6c5e9688142
Екатерина <katafedorovskih53@gmail.com>
2026-09-24 17:41:25 +0500

    Task6

---
+++ Tyuiu.FedorovskikhEO.Sprint1.Task6.V5.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.FedorovskikhEO.Sprint1.Task6.V5.Lib
+{
+    public class DataService : ISprint1Task6V5
+    {
+        public string CheckSymmetricalWords(string value)
+        {
+            string result="";
+            string[] words = value.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                string reverse = "";
+                for (int j = word.Length - 1; j >= 0; j--)
+                {
+                    reverse += word[j];
+                }
+                if (word == reverse)
+                {
+                    result += word + " ";
+                }
+
+            }
+            return result.Trim();
+
+
+        }
+    }
+}
---
+++ Tyuiu.FedorovskikhEO.Sprint1.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using Tyuiu.FedorovskikhEO.Sprint1.Task6.V5.Lib;
+namespace Tyuiu.FedorovskikhEO.Sprint1.Task6.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "казак шалаш привет";
+            DataService ds= new DataService();
+            string res = ds.CheckSymmetricalWords(strTest);
+            string wait = "казак шалаш";
+            Assert.AreEqual(wait, res);
+        }
+    }
+} 
