95e65ca1b6a803ef0c37f35900f4a4b49678feb4
Екатерина <katafedorovskih53@gmail.com>
2026-09-25 20:57:57 +0500

    Task7

---
+++ Tyuiu.FedorovskikhEO.Sprint1.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.FedorovskikhEO.Sprint1.Task7.V18.Lib;
+namespace Tyuiu.FedorovskikhEO.Sprint1.Task7.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds=new DataService();
+            double x = 3;
+            double y = 2;
+            double wait = 3.397;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FedorovskikhEO.Sprint1.Task7.V18/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.FedorovskikhEO.Sprint1.Task7.V18.Lib;
+namespace Tyuiu.FedorovskikhEO.Sprint1.Task7.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Федоровских Е. O. | ИСТНБ-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Федоровских Екатерина Олеговна | ИСТНБ-26-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* значениям данных, вводимых пользователем                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine(" 1+ sin**2 (x+y)");
+            Console.WriteLine(" ---------------");
+            Console.WriteLine("                    +x ");
+            Console.WriteLine("2 + |(x-2x)/(1+x**2*y**2|");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите  значение х:                                                       ");
+            x=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите  значение y:                                                       ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadKey();
+        }
+    }
+}
